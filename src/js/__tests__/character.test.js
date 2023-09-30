11f36f31709172923e0f9a97f40ada40f2b51762
umniknik <kaaaaaljan@mail.ru>
2023-09-30 17:37:56 +0300

    Диплом «Продвинутый JS» gh-pages готов - уменьшил вложенность if - увеличил количество методов

--- src/js/__tests__/character.test.js
+++
@@ -1,5 +0,0 @@
-import Character from '../Character';
-
-test('Checking for throwing an error when creating a Character class', () => {
-  expect(() => new Character(1, 'bowman')).toThrow(new Error('Нельзя создавать персонажа при помощи new Character()'));
-});
